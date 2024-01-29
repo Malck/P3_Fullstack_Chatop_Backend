d3cb33996e583e0fd2cbab812f30de11e33fd39d
MC <Malckira@gmail.com>
2024-01-29 12:22:49 +0100

    Mise a jour du model pour la nouvelle table RENTALS

--- src/main/java/com/chatop/api/model/Rental.java
+++ src/main/java/com/chatop/api/model/Rental.java
@@ -1,5 +1,56 @@
 package com.chatop.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "RENTALS")  
 public class Rental {
-    
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+	private String name;
+
+	private Double price;
+
+	private Double surface;
+
+	private String description;
+
+	private String picture;
+
+	@Column(name = "created_at")
+	private String createdAt;
+
+	@Column(name = "updated_at")
+	private String updatedAt;
+
+	@Column(name = "owner_id")
+	private Long ownerId;
+	
 }
+
+
+/* 
+public Rental(Long id, String name, Double price, Double surface, String description, String picture,
+			String createdAt, String updatedAt, Long ownerId) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.surface = surface;
+		this.description = description;
+		this.picture = picture;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.ownerId = ownerId;
+	}
+*/
