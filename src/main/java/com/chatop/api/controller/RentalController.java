51f313d68c545804fb3682d2467ec7a724ff3122
MC <Malckira@gmail.com>
2024-01-29 12:25:39 +0100

    Maj du RentalController

--- src/main/java/com/chatop/api/controller/RentalController.java
+++ src/main/java/com/chatop/api/controller/RentalController.java
@@ -1,5 +1,27 @@
+/* V1 construit selon le cours */
+
 package com.chatop.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chatop.api.model.Rental;
+import com.chatop.api.service.RentalService;
+
+@RestController
 public class RentalController {
-    
+
+    @Autowired
+    private RentalService rentalService;
+
+    /**
+    * Read - Get all rentals
+    * @return - An Iterable object of Rental full filled
+    */
+    @GetMapping("/rentals")
+    public Iterable<Rental> getRentals() {
+        return rentalService.getRentals();
+    }
+
 }
