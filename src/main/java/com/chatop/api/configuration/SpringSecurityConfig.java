e8f6705ebd0fa826524fa6c6bbf59a8b863469c7
MC <Malckira@gmail.com>
2024-02-13 16:34:29 +0100

    noAuthFilterChain for register

--- src/main/java/com/chatop/api/configuration/SpringSecurityConfig.java
+++ src/main/java/com/chatop/api/configuration/SpringSecurityConfig.java
@@ -4,13 +4,16 @@ import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -21,7 +24,6 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 
-
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 @Configuration
@@ -29,11 +31,23 @@ import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 public class SpringSecurityConfig {
 
-	
-
     private String jwtKey = "laclegeneree256....";
 
+	// public static final String[] allowedRoutes = {"/auth/register", "/auth/login", "/images/**", "/swagger-ui/**", "/v3/api-docs/**"};
+
 	@Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+	@Bean
+	@Order(1)
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
 		    .csrf(csrf -> csrf.disable())
@@ -42,6 +56,21 @@ public class SpringSecurityConfig {
 			.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
 			.httpBasic(Customizer.withDefaults()).build();
 	}
+
+	@Bean
+    @Order(2)
+    public SecurityFilterChain noAuthFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.antMatcher("/auth/register")
+				.authorizeHttpRequests(auth -> {
+					auth.anyRequest().permitAll(); // Autoriser l'accès sans authentification
+				})
+				.build();
+	}
+	
+	
 
 	@Bean
 	public JwtEncoder jwtEncoder() {
@@ -58,18 +87,7 @@ public class SpringSecurityConfig {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-    @Bean
-    public AuthenticationManager authenticationManager(
-            UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-        return new ProviderManager(authenticationProvider);
-    }
-	 
-	
+    
 
 	/* 
 	@Autowired CustomService customService;
@@ -82,17 +100,31 @@ public class SpringSecurityConfig {
             auth.anyRequest().authenticated();
         }).formLogin(Customizer.withDefaults()).build();
 	}
+
+	// @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         // Security on requests
+    //         .authorizeHttpRequests((request) -> {
+    //             for (String allowedRoute : allowedRoutes) {
+    //                 request.requestMatchers(allowedRoute).permitAll();
+    //             }
+    //             request.anyRequest().authenticated();
+    //         })
+
+    //         // Stateless session
+    //         .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+    //         // CSRF disabled because stateless session
+    //         .csrf(AbstractHttpConfigurer::disable)
+
+    //         // Custom Jwt Authentification filter
+    //         .authenticationProvider(authenticationProvider()).addFilterBefore(
+    //                 jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+    //         ;
+            
+    //     return http.build();
+    // }
 	
-	@Bean
-	public UserDetailsService users() {
-		UserDetails user = User.builder()
-				.username("user")
-				.password(passwordEncoder().encode("user"))
-				.roles("USER").build();
-		UserDetails admin = User.builder()
-				.username("admin")
-				.password(passwordEncoder().encode("admin"))
-				.roles("USER", "ADMIN").build();
-		return new InMemoryUserDetailsManager(user, admin);
-	}*/
+	*/
 }
