4b71dc7493d78b2aa5fb24f2cd0bac55f87bc09e
MC <Malckira@gmail.com>
2024-02-26 12:49:13 +0100

    add UserDTO

---
+++ src/main/java/com/chatop/api/model/UserDTO.java
@@ -0,0 +1,33 @@
+package com.chatop.api.model;
+
+public class UserDTO {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
