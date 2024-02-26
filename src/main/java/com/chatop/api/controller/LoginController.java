5cefdd14f67f49e5f08ccc583ec505d3074d6e09
MC <Malckira@gmail.com>
2024-02-26 12:48:53 +0100

    postmapping resgister add

--- src/main/java/com/chatop/api/controller/LoginController.java
+++ src/main/java/com/chatop/api/controller/LoginController.java
@@ -1,13 +1,25 @@
 package com.chatop.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chatop.api.model.User;
+//import com.chatop.api.model.UserDTO;
 import com.chatop.api.service.JWTService;
+import com.chatop.api.service.MyUserDetailsService;
 
 @RestController
 public class LoginController {
+
+	@Autowired
+    private MyUserDetailsService userDetailsService;
 
 	private JWTService jwtService;
 
@@ -16,9 +28,28 @@ public class LoginController {
 	}
 
 	@PostMapping("/login")
-	public String getToken(Authentication authentication) {
-		String token = jwtService.generateToken(authentication);
-		return token;
+    public String getToken(Authentication authentication) {
+    String token = jwtService.generateToken(authentication);
+    return token;
+
 	}
 
+	@PostMapping("/api/auth/register") 
+	
+	public ResponseEntity<?> saveUser(User userDTO) throws Exception {
+
+		userDetailsService.save(userDTO);
+
+		// Load user details
+		//final CustomUserDetails userDetails = userDetailsService.loadUserByUserEmail(user.getEmail());
+
+		// Generate token
+		//final String token = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok("hello");
+	}
 }
+
+
+
+
