76f4fe4ed76870eabefdfb88bdc79bfebde9cee3
MC <Malckira@gmail.com>
2024-02-26 15:58:32 +0100

    modif on generateToken

--- src/main/java/com/chatop/api/service/JWTService.java
+++ src/main/java/com/chatop/api/service/JWTService.java
@@ -19,13 +19,13 @@ public class JWTService {
 		this.jwtEncoder = jwtEncoder;
 	}
 	
-	public String generateToken(Authentication authentication) {
+	public String generateToken(String authentication) {
         Instant now = Instant.now();
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .issuedAt(now)
                 .expiresAt(now.plus(1, ChronoUnit.DAYS))
-                .subject(authentication.getName())
+                .subject(authentication)
                 .build();
         JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
         return this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
