291d0893739ebc11bc5f80fcf85777b919da342a
MC <Malckira@gmail.com>
2024-04-02 15:44:15 +0200

    Add GetMapping for api/auth/me

--- src/main/java/com/chatop/api/controller/LoginController.java
+++ src/main/java/com/chatop/api/controller/LoginController.java
@@ -1,17 +1,12 @@
 package com.chatop.api.controller;
 
-import java.security.Principal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.chatop.api.model.JwtRequest;
 import com.chatop.api.model.User;
 import com.chatop.api.model.UserDTO;
+import com.chatop.api.repository.UserRepository;
 import com.chatop.api.service.JWTService;
 import com.chatop.api.service.MyUserDetailsService;
+import com.chatop.api.service.UserService;
+
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
 public class LoginController {
@@ -29,10 +29,15 @@ public class LoginController {
 	@Autowired
     private MyUserDetailsService userDetailsService;
 
+	private UserService userService;
+ 
 	private JWTService jwtService;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
+
+	@Autowired
+    private UserRepository userRepository;
 
 	public LoginController(JWTService jwtService) {
 		this.jwtService = jwtService;
@@ -53,7 +58,8 @@ public class LoginController {
 		// Generate token
 		final String token = jwtService.generateToken(userDTO.getEmail());
 
-		return ResponseEntity.ok("token:{" + token + "}") ;
+		//return ResponseEntity.ok("token:{" + token + "}") ;
+		return ResponseEntity.ok(Map.of("token", token));
 	}
 
 
@@ -69,9 +75,19 @@ public class LoginController {
 
 	 	final String token = jwtService.generateToken(authenticationRequest.getEmail());
 
-	 	return ResponseEntity.ok("token:{" + token + "}");
+	 	//return ResponseEntity.ok("token:{" + token + "}");
+		return ResponseEntity.ok(Map.of("token", token));
 	}
-
+    
+	
+	@GetMapping("api/auth/me")
+     public ResponseEntity<UserDTO> me(Principal principal) throws Exception {
+		UserDTO userResponse = new UserDTO(1,"test","test@test.com","2022/02/02","2022/08/02" );
+                  
+        return ResponseEntity.ok(userResponse);
+    }
+	
+	
 }
 
 
